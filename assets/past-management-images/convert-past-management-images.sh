c0dc7fde9b34f3a30bc5cd9d66a416916c5ef8b3
Mark Perman <maperm@taltech.ee>
2025-11-14 13:06:09 +0200

    past management update

--- assets/past-management-images/convert-past-management-images.sh
+++ assets/past-management-images/convert-past-management-images.sh
@@ -2,5 +2,5 @@ mkdir -p optimised
 for file in original/*; do
     echo $file
     base_name=$(basename $file)
-    convert ${file} -resize "200x200^>" -quality 89 optimised/${base_name}
+    convert ${file} -resize "200x200^>" -gravity center -crop 200x200+0+0 -quality 89 optimised/${base_name}
 done
